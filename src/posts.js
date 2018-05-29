f0726d32b054fd4bb50d4b9f0d56275cbf6f0407
chgrp-hung <hung@thechgrp.com>
2018-05-28 19:44:52 -0700

    create edit components added

--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,11 @@
 // in src/posts.js
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+// import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, ReferenceField,
+  Edit, Create, TextField, EditButton, DisabledInput,
+  LongTextInput, ReferenceInput, SelectInput,
+  SimpleForm, TextInput } from 'react-admin';
+
 
 export const PostList = (props) => (
     <List {...props}>
@@ -11,6 +16,36 @@ export const PostList = (props) => (
             </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
 );
